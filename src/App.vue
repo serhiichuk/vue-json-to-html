b6a685bdf3ead94fc191186459901dec27573569
Taras <tarasserhiytschuk@gmail.com>
2018-06-22 17:18:59 +0300

    v0.1.6

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,52 @@
 <template>
-  <div id="app">
-    <JsonToHtml :json="json" :bem="true" rootClassName="wrapper" rootTagName="section"/>
-  </div>
+    <div id="app">
+        <JsonToHtml :json="json" :bem="true" rootClassName="wrapper" rootTagName="section">
+            <button @click="doSms">OK</button>
+        </JsonToHtml>
+    </div>
 </template>
 
 <script>
-import JsonToHtml from "./components/json-to-html";
+    import JsonToHtml from "./components/json-to-html";
 
-export default {
-  components: {
-    JsonToHtml
-  },
-  data() {
-    return {
-      json: {
-        title: 'True Title',
-        'oredered-list': [
-          'list item - 1',
-          'list item - 2',
-          'list item - 3',
-          'list item - 4',
-          {
-            'extended': {
-              name: 'foo',
-              desc: 'bar'
+    export default {
+        components: {
+            JsonToHtml
+        },
+        data() {
+            return {
+                json: {
+                    title: 'True Title',
+                    'oredered-list': [
+                        'list item - 1',
+                        'list item - 2',
+                        'list item - 3',
+                        'list item - 4',
+                        {
+                            'extended': {
+                                name: 'foo',
+                                desc: 'bar'
+                            }
+                        }
+                    ]
+                }
             }
-          }
-        ]
-      }
+        },
+        methods: {
+            doSms() {
+                console.log('OK');
+            }
+        }
     }
-  }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/json-to-html.vue
+++ src/components/json-to-html.vue
@@ -1,127 +1,129 @@
 <script type="text/jsx">
-  export default {
-    name: "json-to-html",
-    props: {
-      json: {
-        type: Object,
-        required: true
-      },
-      bem: {
-        type: Boolean,
-        default: false
-      },
-      rootClassName: {
-        type: String,
-        default: undefined
-      },
-      rootTagName: {
-        type: String,
-        default: 'div'
-      },
-      inheritClassName: {
-        type: Boolean,
-        default: true
-      }
-    },
+    export default {
+        name: "json-to-html",
+        props: {
+            json: {
+                type: Object,
+                required: true
+            },
+            bem: {
+                type: Boolean,
+                default: false
+            },
+            rootClassName: {
+                type: String,
+                default: undefined
+            },
+            rootTagName: {
+                type: String,
+                default: 'div'
+            },
+            inheritClassName: {
+                type: Boolean,
+                default: true
+            }
+        },
 
-    created() {
-      if (this.bem && !this.inheritClassName) {
-        console.warn('[Warn]: JsonToDo, conflict props: For using BEM in prop "inheritClassName" ' +
-          'value must be "true", you pass: ' + this.inheritClassName);
-      }
-    },
-
-    /**
-     * This function render DOM three from props data
-     * DOM will rendered such as json structure
-     * Each elements will have necessary classNames from json keys
-     **/
-    render(h) {
-      const json = this.json;
-
-      const getClassName = ({parentClassName, className, isElement}) => {
-        const inheritClassName = this.inheritClassName;
-        if (!inheritClassName) return className;
-
-
-        const isBem = inheritClassName && this.bem;
-        const isNumberedListItem = typeof className === 'number';
-        const sep = isBem
-          ? isElement ? '_' : '__'
-          : '-';
-        const prefixClassName = parentClassName
-          ? parentClassName.split(' ')[0] + sep // get only first class name
-          : '';
-
-        return isNumberedListItem
-          ? parentClassName && parentClassName + ' ' + parentClassName + sep + (className + 1)
-          : prefixClassName + className
-
-      };
-
-      const jsToDOM = ({data, className, parentClassName}) => {
-        const isString = typeof data === 'string' || typeof data === 'number';
-        const isArray = Array.isArray(data);
-        const isObject = typeof data === 'object' && !isArray;
-        let newClassName;
+        created() {
+            if (this.bem && !this.inheritClassName) {
+                console.warn('[Warn]: JsonToDo, conflict props: For using BEM in prop "inheritClassName" ' +
+                    'value must be "true", you pass: ' + this.inheritClassName);
+            }
+        },
 
         /**
-         *  return div single element
-         */
-        if (isString) {
-          newClassName = getClassName({
-            parentClassName,
-            className,
-            isElement: true
-          });
+         * This function render DOM three from props data
+         * DOM will rendered such as json structure
+         * Each elements will have necessary classNames from json keys
+         **/
+        render(h) {
+            const json = this.json;
 
-          return <div class={newClassName} domPropsInnerHTML={data}/>
+            const getClassName = ({parentClassName, className, isElement}) => {
+                const inheritClassName = this.inheritClassName;
+                if (!inheritClassName) return className;
+
+
+                const isBem = inheritClassName && this.bem;
+                const isNumberedListItem = typeof className === 'number';
+                const sep = isBem
+                    ? isElement ? '_' : '__'
+                    : '-';
+                const prefixClassName = parentClassName
+                    ? parentClassName.split(' ')[0] + sep // get only first class name
+                    : '';
+
+                return isNumberedListItem
+                    ? parentClassName && parentClassName + ' ' + parentClassName + sep + (className + 1)
+                    : prefixClassName + className
+
+            };
+
+            const jsToDOM = ({data, className, parentClassName}) => {
+                const isString = typeof data === 'string' || typeof data === 'number';
+                const isArray = Array.isArray(data);
+                const isObject = typeof data === 'object' && !isArray;
+                let newClassName;
+
+                /**
+                 *  return div single element
+                 */
+                if (isString) {
+                    newClassName = getClassName({
+                        parentClassName,
+                        className,
+                        isElement: true
+                    });
+
+                    return <div class={newClassName} domPropsInnerHTML={data}/>
+                }
+
+                /**
+                 * call jsToDOM() for each element in array/object recursive
+                 */
+                newClassName = getClassName({parentClassName, className});
+
+                if (isArray) {
+                    return <div class={newClassName}>
+                        {data.map((item, index) => jsToDOM({
+                                data: item,
+                                parentClassName: getClassName({parentClassName, className}),
+                                className: index
+                            })
+                        )}
+                    </div>;
+                }
+
+                if (isObject) {
+                    return <div class={newClassName}>
+                        {Object.keys(data).map(key => jsToDOM({
+                                data: data[key],
+                                parentClassName: getClassName({parentClassName, className}),
+                                className: key
+                            })
+                        )}
+                    </div>
+                }
+            };
+
+
+            /**
+             * Create root element and call jsToDOM() for each json element
+             */
+            const root = {
+                tag: this.rootTagName,
+                class: this.rootClassName
+            };
+
+            const dom = Object.keys(json).map(item => jsToDOM({
+                data: json[item],
+                className: item,
+                parentClassName: root.class
+            }));
+
+            dom.push(this.$slots.default);
+
+            return h(root.tag, {class: root.class}, dom);
         }
-
-        /**
-         * call jsToDOM() for each element in array/object recursive
-         */
-        newClassName = getClassName({parentClassName, className});
-
-        if (isArray) {
-          return <div class={newClassName}>
-            {data.map((item, index) => jsToDOM({
-                data: item,
-                parentClassName: getClassName({parentClassName, className}),
-                className: index
-              })
-            )}
-          </div>;
-        }
-
-        if (isObject) {
-          return <div class={newClassName}>
-            {Object.keys(data).map(key => jsToDOM({
-                data: data[key],
-                parentClassName: getClassName({parentClassName, className}),
-                className: key
-              })
-            )}
-          </div>
-        }
-      };
-
-
-      /**
-       * Create root element and call jsToDOM() for each json element
-       */
-      const root = {
-        tag: this.rootTagName,
-        class: this.rootClassName
-      };
-
-      return h(root.tag,
-        {class: root.class},
-        Object.keys(json).map(item => jsToDOM({
-          data: json[item],
-          className: item,
-          parentClassName: root.class
-        })));
-    }
-  };
+    };
 </script>
